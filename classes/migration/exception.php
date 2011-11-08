59b17ad4bf0c3f81e8d6f5a9cc3f67eea5a3476f
Ivan Kerin <ikerin@gmail.com>
2011-11-08 19:42:13 +0200

    renaming files to change the case

---
+++ classes/migration/exception.php
@@ -0,0 +1,10 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+/**
+ * Migration exceptions.
+ *
+ * @package    OpenBuildings/timestamped-migrations
+ * @author     Ivan K
+ * @copyright  (c) 2011 OpenBuildings
+ * @license    http://creativecommons.org/licenses/by-sa/3.0/legalcode
+ */
+class Migration_Exception extends Kohana_Exception {}
