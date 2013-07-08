a761078925b1fbf1b21c11699d52fb9090ae16d1
Ivan Kerin <ivan@openbuildings.com>
2013-07-08 13:02:17 +0300

    add travis

---
+++ tests/bootstrap.php
@@ -0,0 +1,49 @@
+<?php 
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+Kohana::modules(array(
+	'database'             => MODPATH.'database',
+	'timestamp-migrations' => __DIR__.'/..',
+));
+
+function test_autoload($class)
+{
+	$file = str_replace('_', '/', $class);
+
+	if ($file = Kohana::find_file('tests/classes', $file))
+	{
+		require_once $file;
+	}
+}
+
+spl_autoload_register('test_autoload');
+
+Kohana::$config
+	->load('database')
+		->set('default', array(
+			'type'       => 'MySQL',
+			'connection' => array(
+				'hostname'   => 'localhost',
+				'database'   => 'test-timestamp-migrations',
+				'username'   => 'root',
+				'password'   => '',
+				'persistent' => TRUE,
+			),
+			'table_prefix' => '',
+			'charset'      => 'utf8',
+			'caching'      => FALSE,
+		))
+		->set(Kohana::TESTING, array(
+			'type'       => 'MySQL',
+			'connection' => array(
+				'hostname'   => 'localhost',
+				'database'   => 'test-timestamp-migrations',
+				'username'   => 'root',
+				'password'   => '',
+				'persistent' => TRUE,
+			),
+			'table_prefix' => '',
+			'charset'      => 'utf8',
+			'caching'      => FALSE,
+		));
